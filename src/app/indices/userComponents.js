641705a77ffd8ab5d7d9d987d3094890570bdce7
Alexander Pustovalov <apustovalov@gmail.com>
2020-05-19 15:52:18 +0300

    Remove extra files

--- src/app/indices/userComponents.js
+++
@@ -1,2 +0,0 @@
-
-export default {'usr':{'data-display':{'ListNavigation':require('usr/data-display/ListNavigation.comp').default,},'layout':{'Box':require('usr/layout/Box.comp').default,'Container':require('usr/layout/Container.comp').default,'DivWithStyle':require('usr/layout/DivWithStyle.comp').default,'GridWithCells':require('usr/layout/GridWithCells.comp').default,},'page':{'PageFrameWithDrawer':require('usr/page/PageFrameWithDrawer.comp').default,},},};
--- src/app/schema-prod/routes.js
+++
@@ -1,2 +0,0 @@
-
-export default [{"path":"/","pageName":"test-1"},{"path":"/test-1","pageName":"test-1"},{"path":"/test","pageName":"test"}];
