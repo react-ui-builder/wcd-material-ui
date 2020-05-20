b72dd4975f123644aecc9c6c0f2f55df25948798
reactuibuilder <reactuibuilder@gmail.com>
2020-05-20 16:22:49 +0300

    Add Stack component

---
+++ src/usr/layout/Hidden.comp.tsx
@@ -0,0 +1,49 @@
+import React, { ReactNode } from 'react';
+import PropTypes from 'prop-types';
+import HiddenMUI from '@material-ui/core/Hidden';
+
+export interface HiddenProps {
+  xsDown?: boolean;
+  visibilityIn?: '' |
+  'xsDown' | 'smDown' | 'mdDown' | 'lgDown' | 'xlDown' |
+  'xsUp' | 'smUp' | 'mdUp' | 'lgUp' | 'xlUp';
+  content?: ReactNode;
+}
+
+/**
+ * Quickly and responsively toggle the visibility value of components and more with the hidden utilities.
+ */
+class Hidden extends React.Component<HiddenProps, any> {
+
+  static propTypes: PropTypes.InferProps<HiddenProps>;
+  static defaultProps: PropTypes.InferProps<HiddenProps>;
+
+  render(): JSX.Element {
+    const { visibilityIn, content } = this.props;
+    let newProps: any = {};
+    if (visibilityIn) {
+      newProps[visibilityIn] = true;
+    }
+    return (
+        <HiddenMUI {...newProps}>
+          {content}
+        </HiddenMUI>
+    );
+  }
+}
+
+Hidden.propTypes = {
+  visibilityIn: PropTypes.oneOf([
+    '',
+    'xsDown', 'smDown', 'mdDown', 'lgDown', 'xlDown',
+    'xsUp', 'smUp', 'mdUp', 'lgUp', 'xlUp',
+  ]),
+  content: PropTypes.node,
+};
+
+Hidden.defaultProps = {
+  visibilityIn: '',
+  content: <span />
+};
+
+export default Hidden;
