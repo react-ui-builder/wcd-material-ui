6969fcf4d46ba3d9d7cd456e828de9189115d8d8
Dmytro Medzatiy <d.medzatiy@gmail.com>
2020-05-21 11:39:56 +0300

    add dateTimePicker

---
+++ src/usr/inputs/DateTimePicker.comp.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+    DatePicker,
+    TimePicker,
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import { DateTimePickerProps, DateTimePickerTypes } from './DateTimePicker.props';
+
+/**
+ * Date / Time pickers
+ * Date pickers and Time pickers provide a simple way to select a single value from a pre-determined set.
+ */
+
+const DateTimePicker = (props: DateTimePickerProps): JSX.Element => {
+    const { autoOk, format, initialDate, label, variant, disabled, allowKeyboard,
+        allowTextInput, orientation, isTimePicker, onChange} = props;
+    const initialValue = initialDate ? new Date(initialDate) : new Date();
+
+    const [selectedDate, setSelectedDate] = React.useState<Date | null>(
+        initialValue,
+    );
+
+    const handleDateChange = (date: Date | null) => {
+        setSelectedDate(date);
+        if (onChange && date) {
+            onChange(date);
+        }
+    };
+
+    const commonProps = {
+        autoOk,
+        variant,
+        format,
+        id: isTimePicker ? "time-picker" : "date-picker",
+        label,
+        value: selectedDate,
+        disabled,
+        orientation,
+        onChange: handleDateChange,
+    };
+
+    let pickerElement;
+    if (isTimePicker) {
+        if (allowTextInput) {
+            pickerElement = (
+                <KeyboardTimePicker
+                    {...commonProps}
+                    KeyboardButtonProps={{
+                        'aria-label': 'change time',
+                    }}
+                />
+            )
+        } else {
+            pickerElement = (
+                <TimePicker
+                    {...commonProps}
+                />
+            )
+        }
+    } else {
+        if (allowTextInput) {
+            pickerElement = (
+                <KeyboardDatePicker
+                    {...commonProps}
+                    allowKeyboardControl={allowKeyboard}
+                    KeyboardButtonProps={{
+                        'aria-label': 'change date',
+                    }}
+                />
+            )
+        } else {
+            pickerElement = (
+                <DatePicker
+                    {...commonProps}
+                />
+            )
+        }
+    }
+    return (
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            {pickerElement}
+        </MuiPickersUtilsProvider>
+    );
+};
+
+DateTimePicker.propTypes = DateTimePickerTypes;
+
+DateTimePicker.defaultProps = {
+    variant: 'dialog',
+};
+
+export default DateTimePicker;
---
+++ src/usr/inputs/DateTimePicker.props.ts
@@ -0,0 +1,64 @@
+import PropTypes from 'prop-types';
+
+export interface DateTimePickerProps {
+    allowKeyboard?: boolean;
+    allowTextInput?: boolean;
+    autoOk?: boolean;
+    disabled?: boolean;
+    format?: string;
+    initialDate?: string;
+    isTimePicker?: boolean;
+    label?: string;
+    orientation?: 'portrait' | 'landscape';
+    variant: 'inline' | 'dialog' | 'static';
+    onChange: (date: Date) => void;
+}
+
+export const DateTimePickerTypes: PropTypes.InferProps<DateTimePickerProps> = {
+    /**
+     * Enable keyboard input
+     */
+    allowKeyboard: PropTypes.bool,
+    /**
+     * If true, the calendar icon will be shown
+     */
+    allowTextInput: PropTypes.bool,
+    /**
+     * if true the date will be accepted on change
+     */
+    autoOk: PropTypes.bool,
+    /**
+     * If true, the picker will be disabled.
+     */
+    disabled: PropTypes.bool,
+    /**
+     * Date format string
+     */
+    format: PropTypes.string,
+    /**
+     * Initial date/time value
+     */
+    initialDate: PropTypes.string,
+    /**
+     * If true, only time will be shown
+     */
+    isTimePicker: PropTypes.bool,
+    /**
+     * DateTimePicker label text
+     */
+    label: PropTypes.string,
+    /**
+     * Orientation
+     */
+    orientation: PropTypes.oneOf(['portrait', 'landscape']),
+    /**
+     * The variant to use.
+     */
+    variant: PropTypes.oneOf(['inline', 'dialog', 'static']).isRequired,
+    /**
+     * Triggered when the user change the date.
+     * Signature: `(date: Date) => void;`
+     *
+     */
+    onChange: PropTypes.func,
+};
